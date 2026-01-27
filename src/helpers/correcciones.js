7d9bd5281e0607dd5e7c48721dfbb0c17edc47f1
Manuel Villarreal <manolovillarreal@hotmail.com>
2026-01-27 10:48:39 -0500

    update: se modifico el sistema de correcciones para:  - ususario puedan responder solicitudes rechazadas

--- src/helpers/correcciones.js
+++ src/helpers/correcciones.js
@@ -1,13 +1,20 @@
 import contexto from "../contexto/contexto.js";
 
+export const EstadoCorreccion = {
+  PENDIENTE: 1,
+  RESPONDIDA: 2,
+  ACEPTADA: 3,
+  RECHAZADA: 4
+};
+
 export function puedeSolicitarCorrecion(documento){
     const estadosAtencionPermitidos = [5,4];
-    const rolesConPermiso = ["Admisiones", "Auditor"];
+    const rolesConPermiso = ["Admisiones", "Auditoria"];
     const {perfil} = contexto;
     const {atencion,usuario: usuarioDoc} = documento;
 
     const puedeSolicitar = estadosAtencionPermitidos.includes(atencion.estadoAtencionId) 
-                          && rolesConPermiso.includes(perfil.rol.nombre)
-                          && !documento.puedeCargar; 
+                          && rolesConPermiso.includes(perfil.rol.nombre);
+                          
     return puedeSolicitar;
   }
